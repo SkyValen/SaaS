eab5fde0b5852b25cf4c557a110200db1124f9b2
Vladislav <vladislav.boiko@tptlive.ee>
2026-03-05 10:34:09 +0200

    add frontend

---
+++ Frontend/index.js
@@ -0,0 +1,17 @@
+const epxress = require('express');
+const app = express();
+
+let NIMI = process.env.TEAM_NAME || "Unknown Team";
+let PORT = process.env.PORT || 3000;
+
+app.get("/api/info", (req, res) => {
+    res.status(200).json({
+        missioon: "Iseseisev deploimine edukas",
+        meeskond: NIMI,
+        aeg: new Date().toISOString()
+    });
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Api Server töötab selle pordi peale${PORT}`);
+})
