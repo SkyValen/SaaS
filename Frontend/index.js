81e27d3aa83d006c0d790b1a7a5cac6540590b3a
Vladislav <vladislav.boiko@tptlive.ee>
2026-03-05 13:43:43 +0200

    login page

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const path = require("path");
 const app = express();
+const { auth } = require("express-openid-connect")
 
 let NIMI = process.env.TEAM_NAME || "Unknown Team";
 let PORT = process.env.PORT || 3000;
+
+app.use(
+    auth({
+        authRequired: false,
+        auth0Logout: true,
+        secret: process.env.AUTH0_SECRET,
+        baseURL: process.env.AUTH0_BASE_URL,
+        clientID: process.env.AUTH0_CLIENT_ID,
+        issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
+    })
+)
 
 app.get("/api/info", (req, res) => {
     res.status(200).json({
@@ -15,7 +27,9 @@ app.get("/api/info", (req, res) => {
 
 app.get("/", (req, res) => {
     console.log(req.body)
-    res.sendFile(path.join(__dirname, "index.html"))
+    req.oidc.isAuthenticated()
+        ? res.sendFile(path.join(__dirname, "index.html"))
+        : res.send(`Somebody is already logged in`)
 })
 
 app.listen(PORT, '0.0.0.0', () => {
