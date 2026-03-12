03fc1a9d3a120a673f5c3b6610ef91a80e8ffac2
Vladislav <vladislav.boiko@tptlive.ee>
2026-03-12 14:53:54 +0200

    update

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require("path");
-const PocketBase = require('pocketbase').PocketBase;
+const PocketBase = require("pocketbase/cjs");
 
 const app = express();
 const NIMI = process.env.TEAM_NAME || "Unknown Team";
